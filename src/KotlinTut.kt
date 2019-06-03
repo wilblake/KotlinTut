ea6cd9ad3d334f38d0cf04ae92031a447bfc29c7
wil <wblake@emsys.net>
2019-06-03 00:23:28 -0400

    Strings

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -10,4 +10,8 @@ fun main (args: Array<String>) {
     println("Strings Equal: ${str1.equals(str2)}")
     println("Compare A to B: ${"A".compareTo("B")}")
 
+    println("2nd Index : ${str1.get(2)}")
+    println("2nd Index : ${str1[2]}")
+    println("2nd - 7th Index : ${str1.subSequence(2,8)}")
+    println("Contains random : ${str1.contains("random")}")
 }
