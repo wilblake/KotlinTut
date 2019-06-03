80e9b2845ad48505abc9cb75699307545391787c
wil <wblake@emsys.net>
2019-06-03 00:40:57 -0400

    Ranges

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -1,26 +1,15 @@
 package demo
 fun main (args: Array<String>) {
 
-   var myArray = arrayOf(1,1.23,"Doug")
 
-    println(myArray[2])
+val oneTo10 = 1..10
+val alpha = "A".."Z"
+println("R in Alpha: ${"R" in alpha}")
+val tenTo1 = 10.downTo(1)
+val twoTo20 = 2.rangeTo(20)
+val rng3 = oneTo10.step(3)
 
-    myArray[1] = 3.14
-    println("Array Length: ${myArray.size} ")
-
-    println("Doug in Array  : ${myArray.contains("Doug")}")
-
-    var partArray = myArray.copyOfRange(0,1)
-
-    println("First: ${myArray.first()}")
-
-    println("Doug Index : ${myArray.indexOf("Doug")}")
-
-    var sqArray = Array(5,{x -> x * x})
-    println(sqArray[2])
-
-    var arr2: Array<Int> = arrayOf(1,2,3)
-
-
+ for (x in rng3) println("rng3 : $x")
+for (x in tenTo1.reversed() ) println("Reverse: $x")
 
 }
