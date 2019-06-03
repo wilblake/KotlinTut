4ed34c72fb083277a610d7cbec920550e695a60e
wil <wblake@emsys.net>
2019-06-03 00:02:46 -0400

    Second Pass

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -37,4 +37,9 @@ fun main (args: Array<String>)
     }
  var letterGrade: Char = 'A'
     println("A is a Char: ${letterGrade is Char}")
+
+    println("3.14 to Int " + (3.14.toInt()))
+
+    println("A to Int : " + ('A'.toInt()))
+    println("65 to Char: " + 65.toChar() )
 }
