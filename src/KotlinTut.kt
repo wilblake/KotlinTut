e49714fdbd22c7636b4d3cc74b791287e8626ea2
wil <wblake@emsys.net>
2019-06-03 22:11:15 -0400

    functions2 Adv

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -1,35 +1,26 @@
 package demo
-
+import java.util.Random
 
 fun main ( args: Array<String>) {
+    println("Sum = ${getSum(1,2,3,4,5)}")
 
-    fun add(num1: Int, num2: Int): Int = num1 + num2
-    println("5 + 4 = ${add(5, 4)}")
 
-    // Single line function doesn't need return type
-    // Note default values
-    fun subtract(num1: Int = 1, num2: Int = 1) = num1 - num2
-    println("5 - 4 = ${subtract(5, 4)}")
+// function literal
+val multiply = {num1: Int, num2: Int -> num1 * num2}
 
-    // Named params
-    println("4 - 5 = ${subtract(num2 = 5, num1 = 4)}")
+    println("5*3 = ${multiply(5,3)}")
 
-    // Unit , not void
-    fun sayHello(name: String): Unit = println("Hello $name")
-
-    sayHello("Paul")
-
-    val (two, three) = nextTwo(1)
-    println("1 $two $three")
-}
-
-fun nextTwo(num:Int): Pair<Int, Int> {
-    return Pair(num+1,num+2)
 
 }
 
+// variable args
+fun getSum( vararg nums: Int ) : Int {
 
+    var sum = 0
 
+    nums.forEach {n -> sum += n}
+    return sum
+}
 
 
 
